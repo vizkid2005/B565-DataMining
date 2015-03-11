5e44c76a20578ce1417aac21da15e816a02c1178
vizkid2005 <vizkid2005@gmail.com>
2015-03-11 04:04:42 -0400

    Revert "Revert "First Commit of Working Directory""
    This reverts commit 64397276496bae27129508e7cea857938756853a.

---
+++ DataMiningAssignments/src/HW2/Representative.java
@@ -0,0 +1,13 @@
+package HW2;
+
+public class Representative {
+
+    double age;
+    double profession;
+
+    public Representative(double age, double profession){
+        this.age = age;
+        this.profession = profession;
+
+    }
+}
