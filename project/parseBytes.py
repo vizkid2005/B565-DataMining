db835df0b1a7cf788ca95e298cbde7ab7ec018d3
vizkid2005 <vizkid2005@gmail.com>
2015-03-29 19:07:54 -0400

    constructing dictionary

--- project/parseBytes.py
+++ project/parseBytes.py
@@ -1,27 +1,38 @@
 #This file parses the bytes file.
 import sys
 import csv
-from os import listdir
+import os
+
+allTokens = {}
 
 def main():
-	mypath = "/home/vizkid2005/Desktop/kaggle/"
-	allFiles =  listdir(mypath)
+	mypath = os.getcwd()+"/"+"bytefiles/"
+	allFiles =  os.listdir(mypath)
 	for f in allFiles:
 		if ".py" not in f  and "bytes" in f:
-			with open(mypath+f,"r") as myfile:
+			with open(mypath+"/"+f,"r") as myfile:
 				allLines = myfile.readlines()
 				allLines = [line.split(" ") for line in allLines]
 				cleanLines(allLines)
 
+	print len(allTokens.keys())
+				
 def cleanLines(allLines):
-	
+	cleanedLines = []
+
 	for line in allLines:
 		line = line[1:]
 		lastElem = line[-1]
 		lastElem = lastElem[:len(lastElem)-2]
 		line[-1] = lastElem
-		print line	
-		break
-
+		cleanedLines.append(line)
+		for byte in line:
+			if byte not in allTokens:
+				allTokens[byte] = 1
+			else:
+				value = allTokens[byte]
+				allTokens[byte] = value + 1	
+		#print line
+		
 if __name__ == "__main__" : main()
 
